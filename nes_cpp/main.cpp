39842088d3924e8a78e4e870d3c831f0ec78903f
Matthew Brennan Jones <mattjones@workhorsy.org>
2012-10-06 15:01:14 -0700

    Made the program stop with a ctrl+c.

--- nes_cpp/main.cpp
+++ nes_cpp/main.cpp
@@ -2,6 +2,7 @@
 
 
 #include "Globals.h"
+#include <signal.h>
 
 using namespace std;
 
@@ -15,24 +16,31 @@ void nes_CheckForQuitting() {
 	}
 }
 
+void exit_handler(int signum) {
+	exit(signum);
+}
+
 int main(int argc, char* argv[]) {
-	/* Initialize SDL, exit if there is an error. */
+	// Initialize SDL, exit if there is an error.
 	if( SDL_Init(SDL_INIT_VIDEO) < 0 ) {
 		fprintf(stderr, "Could not initialize SDL: %s\n", 
 			SDL_GetError());
 		return -1;
 	}
 	
-	/* When the program is through executing, call SDL_Quit */
-	atexit(SDL_Quit);
-	
-	/* Grab a surface on the screen */
+	// Grab a surface on the screen
 	Globals::sdl_screen = SDL_SetVideoMode(256, 240, 32, SDL_SWSURFACE|SDL_ANYFORMAT);
 	if( !Globals::sdl_screen ) {
 		fprintf(stderr, "Couldn't create a surface: %s\n",
 			SDL_GetError());
 		return -1;
 	}
+	
+	// When the program is through executing, call SDL_Quit
+	atexit(SDL_Quit);
+	
+	// Make ctrl+c exit too
+	signal(SIGINT, exit_handler);
 	
 	vNES* vnes = new vNES();
 	vnes->init();
